66c2bc84ad48a452c24fa1a093c1b1795ccafaf5
Michael Douglas <michael.douglas@fm.usp.br>
2021-11-08 18:32:05 -0300

    Finalização do projeto

--- routes/login/users.php
+++ routes/login/users.php
@@ -3,25 +3,6 @@
 use \App\Http\Response;
 use \App\Controller\Login;
 
-//ROTA DE LISTAGEM DE USUÁRIOS
-$obRouter->get('/admin/users', [
-    'middlewares' => [
-        'required-login',
-    ],
-    function($request){
-        return new Response(200, Admin\User::getUsers($request));
-    }
-]);
-
-//ROTA DE CADASTRO DE UM NOVO USUÁRIO
-$obRouter->get('/admin/users/new', [
-    'middlewares' => [
-        'required-login',
-    ],
-    function($request){
-        return new Response(200, Login\User::getNewUser($request));
-    }
-]);
 
 //ROTA DE CADASTRO DE UM NOVO USUÁRIO (POST)
 $obRouter->post('/login/users/new', [
@@ -60,46 +41,5 @@ $obRouter->post('/login/users/newPass', [
     ],
     function($request){
         return new Response(200, Login\User::setNewPassword($request));
-    }
-]);
-
-//ROTA DE EDIÇÃO DE UM NOVO USUÁRIO
-$obRouter->get('/admin/users/{id}/edit', [
-    'middlewares' => [
-        'required-login',
-    ],
-    function($request, $id){
-        return new Response(200, Admin\User::getEditUser($request, $id));
-    }
-]);
-
-//ROTA DE EDIÇÃO DE UM NOVO USUÁRIO (POST)
-$obRouter->post('/admin/users/{id}/edit', [
-    'middlewares' => [
-        'required-login',
-    ],
-    function($request, $id){
-        return new Response(200, Admin\User::setEditUser($request, $id));
-    }
-]);
-
-
-//ROTA DE EXCLUSÃO DE UM NOVO USUÁRIO
-$obRouter->get('/admin/users/{id}/delete', [
-    'middlewares' => [
-        'required-login',
-    ],
-    function($request, $id){
-        return new Response(200, Admin\User::getDeleteUser($request, $id));
-    }
-]);
-
-//ROTA DE EXCLUSÃO DE UM NOVO USUÁRIO (POST)
-$obRouter->post('/admin/users/{id}/delete', [
-    'middlewares' => [
-        'required-login',
-    ],
-    function($request, $id){
-        return new Response(200, Admin\User::setDeleteUser($request, $id));
     }
 ]);
